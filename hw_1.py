ea89a7b2bdf1b25578421a3b6cc023a9192fea1b
Pk2911 <Pk2911@users.noreply.github.com>
2026-06-22 23:50:18 +0530

    hw for 23rd june

--- hw_1.py
+++ hw_1.py
@@ -1,2 +1,16 @@
 # input n words , make it into a sentence
+# n=int(input("enter number of words:"))
+# sentence=""
+# for i in range(n):
+#     word=input("enter your word:")
+#     sentence=sentence+word+" "
+# print(sentence)
+
+
 #reverse of a string without using [::-1]
+
+# word=input("enter your string:")
+# rev=""
+# for i in range(len(word)):
+#     rev=word[i]+rev
+# print(rev)
