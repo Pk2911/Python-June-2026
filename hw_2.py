6187de76007624001c618d6a84726deae8b17f41
Pk2911 <Pk2911@users.noreply.github.com>
2026-06-23 23:09:23 +0530

    turn a list into a string

--- hw_2.py
+++ hw_2.py
@@ -1,1 +1,10 @@
 #turn strings which are in a list into a complete string
+
+
+
+user = input("enter the strings:").split() 
+print(user)
+sentence=""
+for i in user:
+    sentence = sentence + i + " "
+print(sentence)
