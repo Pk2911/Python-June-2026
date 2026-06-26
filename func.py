d9cf66ed49985ed9788699ed279338f970af5be4
Pk2911 <Pk2911@users.noreply.github.com>
2026-06-26 11:50:07 +0530

    changes

---
+++ func.py
@@ -0,0 +1,15 @@
+# a=[1,2,3,4,55,66,77]
+# b=[]
+# c=[]
+# mid=(len(a)+1)//2
+
+# for i in a:
+#     if i>mid:
+#         c.append(i)
+#     else:
+#         b.append(i)
+
+# print(b)
+# print(c)
+        
+
