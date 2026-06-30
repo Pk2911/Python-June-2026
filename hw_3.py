e5b2d7b7e71895840192eb9f096247b56cc9aae6
Pk2911 <Pk2911@users.noreply.github.com>
2026-07-01 00:11:10 +0530

    hw_3

--- hw_3.py
+++ hw_3.py
@@ -1,2 +1,38 @@
 #age calc
-#Bmi calc
+
+# def calculateAge (bd,bm,by,cd,cm,cy):
+#     years=0
+#     for y in range(by,cy):
+#         years+=1
+#     if (cm,cd)<(bm,bd):
+#         years-=1
+
+#     return years
+
+# bd, bm, by = map(int, input("Enter birth day, month, year (separated by spaces): ").split())
+# cd,cm,cy = map(int, input("Enter birth day, month, year (separated by spaces): ").split())
+
+# age = calculateAge(bd,bm,by,cd,cm,cy)
+
+# print(f"Age: {age} years old")
+
+
+#bmi calc
+def check_bmi(w,h):
+    bmi=w/((h/100)**2)
+    if bmi < 18.5:
+        category = "Underweight"
+    elif bmi < 25:
+        category = "Normal"
+    elif bmi < 30:
+        category = "Overweight"
+    else:
+        category = "Obese"
+    return bmi,category
+
+w = float(input("Enter weight: "))
+h = float(input("Enter height: "))
+
+bmi_value, bmi_category = check_bmi(w, h)
+print("BMI:", bmi_value)
+print("Category:", bmi_category)
