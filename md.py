4620f41f5444bd47c806a7601c1d98bf4fc01f7a
Pk2911 <Pk2911@users.noreply.github.com>
2026-07-01 13:01:39 +0530

    1st july

---
+++ md.py
@@ -0,0 +1,11 @@
+name="das"
+
+def add(a,b):
+    return a+b
+
+def factorial(n):
+    fac=1
+    for i in range(1,n+1):
+        fac=fac*i
+    return fac
+# print(factorial(5))
---
+++ modules.py
@@ -0,0 +1,9 @@
+import math
+print(math.sqrt(25))
+print(math.factorial(10))
+print(math.pi)
+
+a=6.3
+b=6.8
+print(math.floor(a))
+print(math.ceil(b))
